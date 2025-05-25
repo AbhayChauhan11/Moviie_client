a17c66ace9b531a7d4163424eeea617b776940aa
AbhayChauhan11 <abychauhan12345@gmail.com>
2025-05-25 22:56:30 +0530

    few changes

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
-
+mongoose.connection.setMaxListeners(20); // To prevent MaxListeners warning
+// Importing the mongoose library to interact with MongoDB
+// and setting the maximum number of listeners to 20 to avoid warnings
+// This is useful when you have multiple event listeners attached to the connection
 export async function connect()
 {
   try {
